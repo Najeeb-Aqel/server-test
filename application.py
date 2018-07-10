f03e68f4a620baef419fa1a6706fa3fb2de72d2c
Najeeb Aqel <najeebmms@hotmail.com>
2018-07-11 00:48:00 +0300

    Update application.py

--- application.py
+++ application.py
@@ -227,4 +227,4 @@ def menuItemJSON(category_id, item_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
